d48b608c58b245c8862125464caf9b2560ae3601
Saad TALBI <talbisaad_007@hotmail.fr>
2016-05-21 01:30:35 +0200

    compelement de beans

--- src/Beans/Classe.java
+++ src/Beans/Classe.java
@@ -1,5 +1,13 @@
 package Beans;
 
 public class Classe {
+	
+	private int IdClasse;
+	private String NomClasse;
+	private String Niveau;
+	private String Moyenne;
+	
+	
+	
 
 }
--- src/Beans/Etudiant.java
+++ src/Beans/Etudiant.java
@@ -1,5 +1,78 @@
 package Beans;
 
+import java.util.Date;
+
 public class Etudiant {
+	private int INE ;
+	private String NomEtudiant;
+	private String PrenomEtudiant;
+	private  Date DateDeNaissance;
+	private String EmailEtudiant;
+	private int TelEtud;
+	private String Niveau;
+	
+	public Etudiant(int iNE, String nomEtudiant, String prenomEtudiant, Date dateDeNaissance, String emailEtudiant,
+			int telEtud, String niveau) {
+		super();
+		INE = iNE;
+		NomEtudiant = nomEtudiant;
+		PrenomEtudiant = prenomEtudiant;
+		DateDeNaissance = dateDeNaissance;
+		EmailEtudiant = emailEtudiant;
+		TelEtud = telEtud;
+		Niveau = niveau;
+	}
+	public Etudiant() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public int getINE() {
+		return INE;
+	}
+	public void setINE(int iNE) {
+		INE = iNE;
+	}
+	public String getNomEtudiant() {
+		return NomEtudiant;
+	}
+	public void setNomEtudiant(String nomEtudiant) {
+		NomEtudiant = nomEtudiant;
+	}
+	public String getPrenomEtudiant() {
+		return PrenomEtudiant;
+	}
+	public void setPrenomEtudiant(String prenomEtudiant) {
+		PrenomEtudiant = prenomEtudiant;
+	}
+	public Date getDateDeNaissance() {
+		return DateDeNaissance;
+	}
+	public void setDateDeNaissance(Date dateDeNaissance) {
+		DateDeNaissance = dateDeNaissance;
+	}
+	public String getEmailEtudiant() {
+		return EmailEtudiant;
+	}
+	public void setEmailEtudiant(String emailEtudiant) {
+		EmailEtudiant = emailEtudiant;
+	}
+	public int getTelEtud() {
+		return TelEtud;
+	}
+	public void setTelEtud(int telEtud) {
+		TelEtud = telEtud;
+	}
+	public String getNiveau() {
+		return Niveau;
+	}
+	public void setNiveau(String niveau) {
+		Niveau = niveau;
+	}
+	
+	
+	
+	
 
 }
